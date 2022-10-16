891966060666101ee3c113662cf0e55fc3556240
Murilo Coelho <murilocoelho08@gmail.com>
2022-10-15 22:50:11 -0300

    ci: Add publish storybook workflow

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,5 +14,12 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinsl: (config, {configType}) => {
+    if(configType === 'PRODUCTION') {
+      config.base = './ignite-lab-design-system/'
+    }
+
+    return config
   }
 }
